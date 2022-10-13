1216cbc912f856030f76be1c5258da4a42abcda8
Andrew Sullivan <603788+asullivanr@users.noreply.github.com>
2022-10-13 18:29:59 -0500

    Create config.cpp

---
+++ addons/Backpacks/config.cpp
@@ -0,0 +1,18 @@
+class CfgPatches
+{
+	class Tenthed_Test_Backpacks {
+		author = "Akira";
+		name = "10th Backpacks";
+		requiredAddons[] = {
+			"OPTRE_Core",
+			"Silvers_Customs"
+		};
+		weapons[] = {};
+		units[] = {};
+	};
+};
+
+class cfgWeapons
+{
+
+};
