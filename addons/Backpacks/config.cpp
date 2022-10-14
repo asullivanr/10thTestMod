8bc0350117f69de3d6a1e4ce2116c6556ac6fb56
Andrew Sullivan <603788+asullivanr@users.noreply.github.com>
2022-10-13 21:06:13 -0500

    Update config.cpp
    added one of the backpacks. Not available ingame yet.

--- addons/Backpacks/config.cpp
+++ addons/Backpacks/config.cpp
@@ -11,7 +11,29 @@ class CfgPatches
 		units[] = {};
 	};
 };
+class cfgVehicles {
+	class OPTRE_UNSC_Rucksack;
+	class OPTRE_ILCS_Rucksack_Black;
 
+	class Tenthed_Test_ANPRC_521_Backback : OPTRE_UNSC_Rucksack {
+		displayName = "[10th] AN/PRC-521 - Test Mod Non-functional";
+		descriptionShort = "AN/PRC-521<br>UNSC Long Range Radio<br>25km Effective Range";
+		picture = "\OPTRE_weapons\backpacks\icons\icon_b_anprc521_ca.paa";
+		faction = "Tenthed_UnitFact_Main";
+		editorCategory = "Tenthed_EditorCategory_Items";
+		editorSubcategory = "Tenthed_EditorSubcategory_Objects_Backpacks";
+		hiddenSelections[] = {
+			"camo",
+			"camo2",
+			"B_Medic",
+			"B_Addons"
+		};
+		hiddenSelectionsTextures[] = {
+			"optre_unsc_units\army\data\soft_backpack_co.paa",
+			"Tenthed_Backpacks\data\10thcompack.paa"
+		};
+	};
+}
 class cfgWeapons
 {
 
